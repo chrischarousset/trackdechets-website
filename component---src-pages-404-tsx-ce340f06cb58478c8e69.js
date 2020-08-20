6edc18c0279408f787ceacc2f5f74329b88130b9
gabinaureche <hello@gabinaureche.com>
2020-08-20 10:47:47 +0200

    Updates

---
+++ component---src-pages-404-tsx-ce340f06cb58478c8e69.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return c}));var o=e("q1tI"),u=e("V65f");function c(n){var t=n.location,e=o.useState(!1),c=e[0];e[1];return o.useEffect((function(){console.log(t)}),[]),!!c&&o.createElement(u.b,null)}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ce340f06cb58478c8e69.js.map
