7dfd955ef45fe93d7001c1040274f659325cbd83
gabinaureche <hello@gabinaureche.com>
2020-12-03 15:01:03 +0100

    Add matomo plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
 const path = require("path");
+const siteUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://trackdechets.beta.gouv.fr/"
+    : "http://localhost:8000";
 
 module.exports = {
   siteMetadata: {
     title: "Trackdéchets | La traçabilité des déchets en toute sécurité",
     description:
       "Trackdéchets a vocation à simplifier la gestion de vos déchets dangereux au quotidien : 0 papier, traçabilité en temps réel, informations regroupées sur un outil unique, vérification des prestataires.",
-    url:
-      process.env.NODE_ENV === "production"
-        ? "https://trackdechets.beta.gouv.fr/"
-        : "http://localhost:8000",
+    url: siteUrl,
   },
   plugins: [
     "gatsby-plugin-react-helmet",
@@ -26,6 +27,16 @@ module.exports = {
       options: {
         name: `partners-logos`,
         path: path.join(__dirname, "content", "partners"),
+      },
+    },
+    {
+      // by default this plugin doesn't track anything when NODE_ENV !== "production"
+      // https://www.gatsbyjs.com/plugins/gatsby-plugin-matomo/
+      resolve: "gatsby-plugin-matomo",
+      options: {
+        siteId: "83",
+        matomoUrl: "https://stats.data.gouv.fr/",
+        siteUrl,
       },
     },
   ],
