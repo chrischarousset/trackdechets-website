3415b80407a782469fa995c13ce80ca6ca81864e
Colin Maudry <colin@maudry.com>
2022-02-01 11:11:28 +0100

    New stats page with iframe to trackdechets-public-stats

--- src/components/pages/Stats.tsx
+++ src/components/pages/Stats.tsx
@@ -32,122 +32,18 @@ export function Stats() {
   return (
     <Layout>
       <Container>
-        <Maintenance>
-          <h2>Page statistiques en maintenance.</h2>
-        </Maintenance>
-        {/* <Section>
-          <SectionHeading>
-            Déchets dangereux traçés dans Trackdéchets
-          </SectionHeading>
-
-          <Grid items={1}>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/7d4e0e4b-d86a-46ae-b3df-20912b75b6ab"
-                frameBorder="0"
-                width="800"
-                height="400"
-                allowTransparency
-              />
-            </GridItem>
-          </Grid>
-
-          <Grid items={1}>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/3c57f70c-fc58-452e-81f1-33dcaa14166d"
-                frameBorder="0"
-                width="800"
-                height="400"
-                allowTransparency
-              />
-            </GridItem>
-          </Grid>
-
-          <Grid items={3}>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/09d175d6-8b38-42f4-9097-dc753d360217"
-                frameBorder="0"
-                width="350"
-                height="250"
-                allowTransparency
-              />
-            </GridItem>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/73664387-3c43-4b38-8b16-4d1d7f71a50c"
-                frameBorder="0"
-                width="350"
-                height="250"
-                allowTransparency
-              />
-            </GridItem>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/750cfc72-6549-48f0-9483-5c9d449d60b1"
-                frameBorder="0"
-                width="350"
-                height="250"
-                allowTransparency
-              />
-            </GridItem>
-          </Grid>
-        </Section>
-
         <Section>
           <SectionHeading>
-            Les utilisateurs et partenaires de Trackdéchets
+            Déchets dangereux tracés dans Trackdéchets
           </SectionHeading>
-
-          <Grid items={2}>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/312b9159-cab7-4a6e-90ff-de04287f4d8a"
+          <iframe
+                src="https://trackdechets-public-stats.osc-secnum-fr1.scalingo.io/"
                 frameBorder="0"
-                width="700"
-                height="400"
+                width="1000"
+                height="3000"
                 allowTransparency
               />
-            </GridItem>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/7fc8a525-95c3-48e0-becb-6bb5668a2868"
-                frameBorder="0"
-                width="350"
-                height="400"
-                allowTransparency
-              />
-            </GridItem>
-          </Grid>
         </Section>
-
-        <Section>
-          <SectionHeading>
-            Sécurisation écosystème déchets dangeureux
-          </SectionHeading>
-
-          <Grid items={2}>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/4eb75bba-f3fe-4617-9038-9eec6b0e1d51"
-                frameBorder="0"
-                width="350"
-                height="250"
-                allowTransparency
-              />
-            </GridItem>
-            <GridItem>
-              <iframe
-                src="https://analytics.trackdechets.beta.gouv.fr/public/question/b8497611-62b7-4cd5-a82e-fbcbf6348ee2"
-                frameBorder="0"
-                width="350"
-                height="250"
-                allowTransparency
-              />
-            </GridItem>
-          </Grid>
-        </Section> */}
       </Container>
     </Layout>
   );
