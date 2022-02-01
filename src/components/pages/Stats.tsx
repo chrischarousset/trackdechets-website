80936e9ec44f8ce2b9ff362896ef2e0eaf3f6d9a
benoitguigal <benoit.guigal@gmail.com>
2022-02-01 16:00:52 +0100

    update iframe URL and center

--- src/components/pages/Stats.tsx
+++ src/components/pages/Stats.tsx
@@ -4,28 +4,10 @@ import { Layout } from "../Layout";
 import { Container } from "../Container";
 import { Section, SectionHeading } from "../Section";
 
-const Grid = styled.div<{ items: number }>`
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  grid-gap: ${(props) => props.theme.spacing(1)};
-
-  & + & {
-    margin-top: ${(props) => props.theme.spacing(2)};
-  }
-
-  @media ${(props) => props.theme.breakpoints.up("large")} {
-    grid-template-columns: repeat(${(props) => props.items}, 1fr);
-  }
-`;
-const GridItem = styled.div`
+const Iframe = styled.iframe`
   display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Maintenance = styled.div`
-  padding-top: 100px;
-  padding-bottom: 100px;
+  margin: auto;
+  background-color: transparent;
 `;
 
 export function Stats() {
@@ -36,13 +18,13 @@ export function Stats() {
           <SectionHeading>
             Déchets dangereux tracés dans Trackdéchets
           </SectionHeading>
-          <iframe
-                src="https://trackdechets-public-stats.osc-secnum-fr1.scalingo.io/"
-                frameBorder="0"
-                width="1000"
-                height="3000"
-                allowTransparency
-              />
+          <Iframe
+            src="https://statistiques.trackdechets.beta.gouv.fr"
+            frameBorder="0"
+            width="1000"
+            height="3000"
+            allowTransparency
+          />
         </Section>
       </Container>
     </Layout>
